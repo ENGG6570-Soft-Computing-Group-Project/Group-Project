36bc19754db37c6ad248ffdf8e5dcfcc6886fec9
y5mei <meiyaowen@gmail.com>
2021-06-04 21:39:35 -0400

    Brute Force Solution Finished

--- ENGG6570_Project/TracePlot.py
+++ ENGG6570_Project/TracePlot.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import rand
 
-
+## TracePlot is a class that can be initialized with
+## 1) list of [x,y] coordinates for tasks
+## 2) list of list of [x,y] coordinates for each robots's trace [[Rb_1_Trace], [Rb_2_Trace], [Rb3_Trace]]
+## People can use filed robotPlot to get a matplot.plt for the trace of each of the robots
 class TracePlot:
     def __init__(self, target_list, robot_tracy_list):
         self.target_list = target_list
@@ -45,7 +48,7 @@ class TracePlot:
         plt.axis(self.axisLimits)
         plt.xlabel("X axis")
         plt.ylabel("Y axis")
-        plt.title("The Trace of Robots")
+        plt.title("The Trace of Robots with Minial Travel")
         x, y = np.asarray(self.target_list).T
         plt.scatter(x, y, color="green", s=100, marker="s")
         return plt
@@ -54,16 +57,23 @@ class TracePlot:
         cmap = [np.random.rand(self.num_robots,) for _ in range(self.num_robots)]
         for i in range(self.num_robots):
             x0,y0 = self.robot_tracy_list[i][0]
-            newplt.plot(x0, y0, color= cmap[i], marker="o", markersize=8)
+            newplt.plot(x0, y0, color= "red", marker="o", markersize=8)
             x, y = np.asarray(self.robot_tracy_list[i]).T
-            plt.plot(x, y, color = cmap[i], marker="o", markersize=3)
+            plt.plot(x, y, color = "red", marker="o", markersize=3)
         return newplt
 
     def getNumRobots(self):
         return self.num_robots
 
 
-
-
-
-
+#############################################################
+### unit test
+#############################################################
+# target = [[2, 0], [0, 2]]
+# robot1 = [[1, 2], [0.5, 2], [0.25, 2], [0.125, 2], [0.0625, 2]]
+# robot2 = [[2, 2], [2, 1], [2, 0.5], [2, 0.25], [2, 0.125]]
+# robot3 = [[5, 5], [5, 4], [5, 3], [2, 3.5], [1.5, 1]]
+# robot_path = [robot1, robot2, robot3]
+# myPlot = TracePlot(target, robot_path)
+# a = myPlot.robotPlot
+# a.show()
--- ENGG6570_Project/main.py
+++ ENGG6570_Project/main.py
@@ -1,11 +1,22 @@
-import TracePlot as TP
+import TracePlot
+import TaskGenerator
+import numpy as np
+import BruteForceSolution as BFS
 
-target = [[2, 0], [0, 2]]
-robot1 = [[1, 2], [0.5, 2], [0.25, 2], [0.125, 2], [0.0625, 2]]
-robot2 = [[2, 2], [2, 1], [2, 0.5], [2, 0.25], [2, 0.125]]
-robot3 = [[5, 5], [5, 4], [5, 3], [2, 3.5], [1.5, 1]]
-robot_path = [robot1, robot2, robot3]
-
-myPlot = TP.TracePlot(target, robot_path)
+tasklist = TaskGenerator.GenerateTask(5,5)
+print("The task list is:")
+print(tasklist)
+robotList = TaskGenerator.GenerateRobot(5,5)
+print("The robot position list is:")
+print(robotList)
+Solution = BFS.BruteForceSolution(tasklist,robotList)
+RobotTrace = Solution.GenerateRobotPath()
+RobotMoveCode = Solution.sorted_arrangement_dist_list[0][0]
+print(RobotMoveCode)
+Solution.getDistanceHistogram()
+myPlot = TracePlot.TracePlot(tasklist, RobotTrace)
+print(myPlot.num_robots)
+print(myPlot.num_target)
+print(myPlot.num_movements)
 a = myPlot.robotPlot
 a.show()
